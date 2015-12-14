ce02c78ff7342586d870886d46686a43d4d8a846
Mark Baggett <mbagget1@utk.edu>
2015-12-14 17:53:12 -0500

    Refactor so that templates are no longer inline

--- short.rb
+++ short.rb
@@ -1,56 +1,27 @@
-require 'rubygems'
-require 'sinatra'
-require 'redis'
+class Short < Sinatra::Base
+  $redis = Redis.new host: 'localhost', port: 6379
 
-$redis = Redis.new host: 'localhost', port: 6379
-
-get '/' do
-  erb :index
-end
-
-post '/' do
-  if (params[:url] =~ URI::regexp).nil?
-    @error = 'Please enter a valid URL.'
+  get '/' do
     erb :index
-  else
-    @token = (Time.now.to_i + rand(36**8)).to_s 36
-    $redis.set "links:#{@token}", params[:url]
-    erb :shortened
+  end
+
+  post '/' do
+    if (params[:url] =~ URI::regexp).nil?
+      @error = 'Please enter a valid URL.'
+      erb :index
+    else
+      @token = (Time.now.to_i + rand(36**8)).to_s 36
+      $redis.set "links:#{@token}", params[:url]
+      erb :shortened
+    end
+  end
+
+  get '/:token/?' do
+    url = $redis.get "links:#{params[:token]}"
+    if url.nil?
+      @error = "This isn't a valid URL."
+    else
+      redirect url
+    end
   end
 end
-
-get '/:token/?' do
-  url = $redis.get "links:#{params[:token]}"
-  if url.nil?
-    @error = "This isn't a valid URL."
-  else
-    redirect url
-  end
-end
-
-__END__
-
-@@ layout
-<doctype html>
-<html>
-<head>
-<title>URL Shortener</title>
-</head>
-<body>
-<%= yield %>
-</body>
-</html>
-
-@@ index
-<h1>URL Shortener</h1>
-<% if @error %>
-  <p><%= @error %></p>
-<% end %>
-<form action='/' method='post'>
-  <input type='url' name='url' size='40' placeholder='Enter your URL' />
-  <input type='submit' value='Shorten' />
-</form>
-
-@@ shortened
-<h1>Congrats!  Your new link is:</h1>
-<p><a href="http://localhost:4567/<%= @token %>">http://localhost:4567/<%= @token %></a></p>
